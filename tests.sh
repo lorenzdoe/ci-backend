3ad717fe8976bebd231f78e6813f5c91b5a55214
lorenz <lorenz.doe@hotmail.com>
2023-05-06 20:54:40 +0200

    login token doing

--- tests.sh
+++ tests.sh
@@ -3,10 +3,13 @@
 # test authenticate
 curl -X GET -H "Authorization: Bearer myToken123" http://localhost:3000/todos
 echo 
+echo "------------------------------"
 
 # without token
+echo "without authorization token - should fail"
 curl -X GET http://localhost:3000/todos
 echo 
+echo "------------------------------"
 
 # create new todo
 curl --location --request POST 'http://localhost:3000/todos' \
@@ -16,8 +19,10 @@ curl --location --request POST 'http://localhost:3000/todos' \
     "name": "Buy milk"
 }'
 echo 
+echo "------------------------------"
 
 # get created todos
 curl --location --request GET 'http://localhost:3000/todos' \
 --header 'Authorization: Bearer myToken123'
-echo 
+echo
+echo "------------------------------"
