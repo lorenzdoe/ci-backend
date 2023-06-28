6f5f23871d11f03f1019a92bbd15fe5a4900be55
Lorenz Doellinger <if21b084@technikum-wien.at>
2023-06-28 21:25:34 +0200

    finished feature toggle

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -26,8 +26,30 @@ const authenticate = function(req, res, next) {
     }
 };
 
+const isAdmin = function(req, res, next) {
+  if(!Object.prototype.hasOwnProperty.call(req.headers,'authorization')){
+    next(createError(401));
+  }
+  else {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    // check if token is authentic
+    try {
+      const decode = jwt.verify(token, process.env.TOKEN_SECRET);
+      if (decode && decode.username == 'admin') {
+        // set decoded username in req body
+        next();
+      } else {
+        next(createError(403));
+      }
+    } catch (err) {
+      next(createError(401));
+    }
+  }
+};
+
 function generateAccessToken(username) {
   return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '3600s' });
 }
 
-module.exports = { authenticate, generateAccessToken };
+module.exports = { authenticate, isAdmin, generateAccessToken };
